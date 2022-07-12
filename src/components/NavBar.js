3d75d4e17b12e764eaefeb59428e9d7046b982e6
Fiezt <dat09t@gmail.com>
2022-07-12 10:29:30 +0700

    yikes

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,32 +1,35 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import '../styles/NavBar.css';
+import "../styles/NavBar.css";
 import logo from "../img/OwlLogoTrans.png";
 export const NavBar = (props) => {
-    return (
-        <nav className="navbar">
-            <div className="navbar-logo">
-                <Link to='home'>
-                    <img id="logo" src={logo} alt="logo" />
+  return (
+    <nav className="navbar">
+      <div className="navbar-logo">
+        <Link to="/">
+          <img id="logo" src={logo} alt="logo" />
+        </Link>
+      </div>
+      <div
+        className="navbar-container"
+        onMouseEnter={props.onHover}
+        onMouseLeave={props.onOut}
+      >
+        <ul className="nav-bar">
+          {props.tabs.map((tab, i) => {
+            return (
+              <li key={i}>
+                <Link to={tab.link} className="navbar-item">
+                  {tab.name}
                 </Link>
-            </div>
-            <div className="menu-icon" onClick={props.onClick}>
-                Menu icon {props.clicked ? "true" : "false"}
-            </div>
-            <div className="navbar-container" onMouseEnter={props.onHover} onMouseLeave={props.onOut}>
-                <ul className={props.clicked ? "nav-bar activate" : "nav-bar"}>
-
-                    {props.tabs.map((tab, i) => {
-                        return (
-                            <li key={i}>
-                                <Link to={tab.link} className="navbar-item">
-                                    {tab.name}
-                                </Link>
-                            </li>
-                        )
-                    })}
-                </ul>
-            </div>
-        </nav>
-    )
-}
+              </li>
+            );
+          })}
+        </ul>
+        {/* <div className="menu-icon" onClick={props.onClick}>
+          {props.clicked ? "true" : "false"}
+        </div> */}
+      </div>
+    </nav>
+  );
+};
